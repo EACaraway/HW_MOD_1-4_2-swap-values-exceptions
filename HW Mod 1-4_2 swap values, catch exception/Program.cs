df2b37ed9581fb2fc224eceb28d2aa3bd9752e5c
Evan Caraway <evan.caraway@outlook.com>
2020-11-24 23:52:10 -0800

    Project Files

---
+++ HW Mod 1-4_2 swap values, catch exception/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HW_MOD_1_4_2_swap_values__catch_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask user for dividend and divisor and then Tryparse to int.
+            Console.Write("Please ender your dividend: ");
+            int.TryParse(Console.ReadLine(), out int dividend);
+            Console.Write("Please ender your divisor: ");
+            int.TryParse(Console.ReadLine(), out int divisor);
+
+            //per HW instructions, swap user dividend and divisor.
+            Swap(ref dividend,ref divisor);
+            
+            Divide(dividend, divisor, out int quotient);
+            Console.WriteLine($"{dividend} / {divisor} = {quotient}");
+            
+        }
+
+        // method that divides and throws an exception if you try to divide by 0.
+        public static int Divide(int a, int b, out int quotient)
+        {
+            try
+            {
+                quotient = a / b;
+                return quotient;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                Console.WriteLine(e.Message);
+                quotient = 0;
+                return quotient;
+            }
+            
+            //if (b != 0)
+            //{
+            //    return a/b;
+            //}
+            //else
+            //{
+            //    throw new System.DivideByZeroException("You can't divide by zero.");
+                
+            //}
+            
+        }
+
+        // use ref to swap values in memory instead of pointing to a value in memory.
+        public static void Swap( ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ HW Mod 1-4_2 swap values, catch exception/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HW MOD 1-4_2 swap values, catch exception")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HW MOD 1-4_2 swap values, catch exception")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4f64c548-468b-437a-865c-82dd97c06196")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
